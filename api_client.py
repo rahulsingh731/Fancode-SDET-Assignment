d14610ec42bc530e8b17c1f69c8c99d048d5db34
Rahul Singh <rahulrocks731@gmail.com>
2024-10-09 23:09:29 +0530

    Final Commit

--- api_client.py
+++ api_client.py
@@ -3,38 +3,23 @@ import requests
 Base_URL = "https://jsonplaceholder.typicode.com"
 
 
-def get_users(url):
+def get_users():
     """Fetch all users from /users endpoint."""
-    response = requests.get(url + "/users")
+    response = requests.get(Base_URL + "/users")
     response.raise_for_status()
     return response.json()
 
 
-def filter_users(users):
-    fancode_list = [user for user in users
-                    if -40 <= float(user['address']['geo']['lat']) <= 5 and 5 <= float(
-            user['address']['geo']['lng']) <= 100]
-    return fancode_list
-
-
-def get_todos(url):
-    response = requests.get(url + "/todos")
+def get_todos():
+    response = requests.get(Base_URL + "/todos")
     response.raise_for_status()
     return response.json()
 
+def filter_fancode_users(users):
+    """Filter users belonging to the FanCode city (lat: -40 to 5, lng: 5 to 100)."""
+    fancode_users = [
+        user for user in users
+        if -40 <= float(user['address']['geo']['lat']) <= 5 and 5 <= float(user['address']['geo']['lng']) <= 100
+    ]
+    return fancode_users
 
-def filter_todos(user, todos):
-    user_todos = [i for i in todos if i['userId'] == user['id']]
-    completed_todos = [todo for todo in user_todos if todo['completed']]
-    if len(completed_todos) == 0:
-        return 0
-    return (len(completed_todos) / len(user_todos)) * 100
-
-
-users = filter_users(get_users(Base_URL))
-todos = get_todos(Base_URL)
-# print(len(users))
-# print(todos[1]['userId'])
-
-for user in users:
-    print(filter_todos(user, todos))
---
+++ test_fancode.py
@@ -0,0 +1,12 @@
+import api_client
+import logics
+
+def fancode_users_completion():
+    users = api_client.get_users()
+    fc_users = api_client.filter_fancode_users(users)
+    todos = api_client.get_todos()
+    user_with_loss_completion = logics.filter_users_fc(users,todos)
+
+    assert len(user_with_loss_completion) == 0, f"Users with low task completion: {user_with_loss_completion}"
+
+fancode_users_completion()
